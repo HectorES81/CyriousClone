6d34bf7e9af94c3d6ef447a6d6dbca3fec089737
Hector Chicas <hect0rchicas@hotmail.com>
2022-03-07 21:12:30 -0500

    text menu working

--- src/RunCyriousClone.java
+++ src/RunCyriousClone.java
@@ -3,24 +3,23 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class RunCyriousClone {
-
+	final static String bar = "=====";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//scanner object will be passed to each function who requires user input
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Welcome to text based CMS Cyrious Clone.");
 		
-		//validOptions array
-		//c = new company
-		//e = new employee
-		//x = exit
-		//ArrayList<Character> validOptionsMain = new ArrayList<Character>(Arrays.asList('c','e','x'));
-		
+		//valid options for main menu
 		LinkedHashMap<Character, String> validOptionsMain = new LinkedHashMap<>();
-		validOptionsMain.put('c', "Company");
-		validOptionsMain.put('e', "Employee Contact");
-		validOptionsMain.put('x', "Exit");
+		validOptionsMain.put('e', "Estimates");		//enter new, list, search, open
+		validOptionsMain.put('w', "Work Orders");  	//enter new, view list, search, open
+		validOptionsMain.put('c', "Company");		//enter new client or vendor, list, search, open
+		validOptionsMain.put('s', "Search");		//search anything
+		validOptionsMain.put('x', "Exit");			//exit
 		
+		//valid options after you're done inside a menu option 
 		LinkedHashMap<Character, String> enterNewOrExit = new LinkedHashMap<>();
 		enterNewOrExit.put('n', "Enter another");
 		enterNewOrExit.put('m', "Exit to main menu");
@@ -30,12 +29,12 @@ public class RunCyriousClone {
 		
 		while(!done) {
 			Character userOption = printMenu(validOptionsMain, input);
-			if(userOption == 'c') {
+			if(userOption == 'c') { //company program
 				Character subOption = 'n';
 				//stay entering new companies until exit to main menu or exit program
 				
 				while(subOption == 'n') {
-					enterNewCompany();
+					companyMenu(input);
 					
 					subOption = printMenu(enterNewOrExit, input);
 					if(subOption == 'm') {
@@ -45,7 +44,7 @@ public class RunCyriousClone {
 						done = true;
 					}
 				}
-			} else if(userOption == 'e') {
+			} else if(userOption == 'e') { //employee program
 				Character subOption = 'n';
 				//stay entering new employees until exit to main menu or exit program
 				
@@ -65,20 +64,20 @@ public class RunCyriousClone {
 				System.out.println("Exiting...");
 				done = true;
 			} else {
-				
+				//should never reach here but leaving this here in case there are unforseen errors
 			}
 		}
 		input.close();
 	}
-
+	
+	//takes acceptable options and scanner object, prints the menu and returns the option selected
 	private static Character printMenu(LinkedHashMap<Character,String> validOptions, Scanner input) {
-		// TODO Auto-generated method stub
 		boolean done = false;
 		
 		while(!done) {
 			System.out.println("Use one of the following options:");
 			for(Map.Entry<Character, String> option : validOptions.entrySet()) {
-				String sep = ",";
+				String sep = ","; //a comma or a space if displaying the last option aka exit
 				if(option.getKey() == 'x') {
 					sep = "";
 				}
@@ -89,22 +88,42 @@ public class RunCyriousClone {
 			if(input.hasNextLine()) {
 				Character optionSelected = input.nextLine().toLowerCase().charAt(0);
 				
+				//only way to exit is to pick a valid option from validOptions
 				if(validOptions.containsKey(optionSelected)) {
 					done = true;
 					return optionSelected;
 				} else {
 					System.out.println("Invalid option! Let's try again...");
 				}
-				
 			}
 		}
 		return null;
 	}
 
-	public static void enterNewCompany() {
-		System.out.println("Enter new COMPANY information...");
-		System.out.println("Entering info...");
-		System.out.println("Finishing the entry...");
+	public static void companyMenu(Scanner input) {
+		Character userOption = null;
+		
+		//enter new client or vendor, list, search, open
+		System.out.println(bar + "Menu options related to companies" + bar);
+		
+		LinkedHashMap<Character, String> companyMenu = new LinkedHashMap<>();
+		companyMenu.put('c', "New Customer");		//enter new, list, search, open
+		companyMenu.put('v', "New Vendor");  	//enter new, view list, search, open
+		companyMenu.put('b', "Browse");		//enter new client or vendor, list, search, open
+		companyMenu.put('s', "Search");		//search anything
+		companyMenu.put('x', "Exit");			//exit
+		
+		userOption = printMenu(companyMenu, input);
+		
+		while(userOption != 'x') {
+			if (userOption == 'c') {
+				System.out.println("New customer menu");
+				
+				userOption = printMenu(companyMenu, input);
+			}
+		} 
+		
+		
 	}
 	
 	public static void enterNewEmployee() {
@@ -112,5 +131,4 @@ public class RunCyriousClone {
 		System.out.println("Entering info...");
 		System.out.println("Finishing the entry...");
 	}
-
 }
